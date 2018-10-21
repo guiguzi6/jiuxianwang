5498a6823f01c04529a228171c099611c6b68d18
guiguzi6 <1430353093@qq.com>
2018-10-21 10:28:33 +0800

    酒仙网更新

--- dist/js/lunbotu.js
+++ dist/js/lunbotu.js
@@ -87,7 +87,7 @@ $(document).ready(function(){
             $(".bigUl2").animate({"left":nowimg*-268},1000)
         }else{
             nowimg=1
-            $(".bbigUl2").css({"left":2*-268},1000)
+            $(".bigUl2").css({"left":2*-268},1000)
             $(".bigUl2").animate({"left":nowimg*-268},1000)
         }
         $(".btnBg2 em").eq(nowimg).addClass('on').siblings().removeClass('on')
@@ -103,15 +103,112 @@ $(document).ready(function(){
 
     timer=setInterval(rightFunc,2000)
 
-    $(".indexTuanList1").mouseenter(function(){
+    $(".indexAdFocusList1").mouseenter(function(){
         clearInterval(timer)
     })
     $(".btnBox .btnBg2").mouseenter(function(){
         clearInterval(timer)
     })
-    $(".indexTuanList1").mouseout(function(){
+    $(".indexAdFocusList1").mouseout(function(){
         clearInterval(timer)
         timer=setInterval(rightFunc,2000)
     })
 })
 
+// 优惠推荐轮播图
+
+$(function(){
+    var nowing = 0;
+    var timer = null;
+    $(".receBoxs  .raceList:first").clone().appendTo(".receBoxs");
+
+    $(".raceListWrap .raceRight").click(function(){
+        if(nowing < 2){
+            nowing++
+            $(".raceListWrap .receBoxs").animate({"left":nowing*-1190},1000);    
+        }
+        $(".rightNavBox span").eq(nowing).addClass("on").siblings().removeClass("on")
+    })
+
+    $(".raceListWrap .raceLeft").click(function(){
+        if(nowing > 0){
+            nowing--
+            $(".raceListWrap .receBoxs").animate({"left":nowing*1190},1000);    
+        }
+        $(".rightNavBox span").eq(nowing).addClass("on").siblings().removeClass("on")
+    })
+
+
+    $(".rightNavBox span").click(function (){
+        nowimg=$(this).index()
+       $(".rightNavBox span").eq(nowimg).addClass("on").siblings().removeClass("on")
+        $(".raceListWrap .receBoxs").animate({"left":nowimg*-1190}, 1000)
+   });
+})
+
+
+//白酒馆轮播图
+$(function(){
+    var nowimg=0;
+    var timer=null;
+    // 克隆第一张图片，并且放到最后
+    $(" bigUl3 li:first").clone().appendTo('.bigUl3')
+    //右按钮业务
+    $(".you").click(rightFunc)
+        function rightFunc(){
+
+        if(nowimg<2){
+            nowimg++
+            $(".bigUl3").animate({"left":nowimg*-210},1000)
+        }else{
+            nowimg=0
+            $(".bigUl3").animate({"left":3*-210},1000,function(){
+                $(".bigUl3").css("left",0)
+
+            })
+        }
+        $(".btnBg3 em").eq(nowimg).addClass('on').siblings().removeClass('on')
+
+    }
+    // 左按钮业务
+    $(".zuo").click(function(){
+        if(nowimg>0){
+            nowimg--
+            $(".bigUl3").animate({"left":nowimg*-210},1000)
+        }else{
+            nowimg=2
+            $(".bigUl3").css({"left":3*-210},1000)
+            $(".bigUl3").animate({"left":nowimg*-210},1000)
+        }
+        $(".btnBg3 em").eq(nowimg).addClass('on').siblings().removeClass('on')
+    })
+    // 小圆点业务
+    $(".btnBg3 em").click(function (){
+         nowimg=$(this).index()
+        $(".btnBg3 em").eq(nowimg).addClass("on").siblings().removeClass("on")
+         $(".bigUl3").animate({"left":nowimg*-268}, 1000)
+    });
+
+    // 自动轮播
+
+    timer=setInterval(rightFunc,2000)
+
+    $(".bannerSlier .imgBox1").mouseenter(function(){
+        clearInterval(timer)
+    })
+    $(".btnBox .btnBg3").mouseenter(function(){
+        clearInterval(timer)
+    })
+    $(".bannerSlier .imgBox1").mouseout(function(){
+        clearInterval(timer)
+        timer=setInterval(rightFunc,2000)
+    })
+})
+
+// 葡萄酒馆轮播图
+$(function(){
+    var nowimg = 0;
+    var timer = 0;
+
+    $(".")
+})
